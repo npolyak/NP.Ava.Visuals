9fffccc85a50178b3107db2d312f76bbc428b03c
npolyak <nick.polyak@verizon.net>
2023-12-22 14:45:17 -0500

    enabled the theme behavior on the whole application. fixed some minor issues with theming.

--- src/Behaviors/ThemeVariantBehavior.cs
+++ src/Behaviors/ThemeVariantBehavior.cs
@@ -8,18 +8,18 @@ namespace NP.Ava.Visuals.Behaviors
     public class ThemeVariantBehavior
     {
         #region TheThemeVariant Attached Avalonia Property
-        public static ThemeVariantEnum GetTheThemeVariant(StyledElement obj)
+        public static ThemeVariantEnum GetTheThemeVariant(AvaloniaObject obj)
         {
             return obj.GetValue(TheThemeVariantProperty);
         }
 
-        public static void SetTheThemeVariant(StyledElement obj, ThemeVariantEnum value)
+        public static void SetTheThemeVariant(AvaloniaObject obj, ThemeVariantEnum value)
         {
             obj.SetValue(TheThemeVariantProperty, value);
         }
 
         public static readonly AttachedProperty<ThemeVariantEnum> TheThemeVariantProperty =
-            AvaloniaProperty.RegisterAttached<ThemeVariantBehavior, StyledElement, ThemeVariantEnum>
+            AvaloniaProperty.RegisterAttached<ThemeVariantBehavior, AvaloniaObject, ThemeVariantEnum>
             (
                 "TheThemeVariant"
             );
@@ -32,7 +32,12 @@ namespace NP.Ava.Visuals.Behaviors
 
         private static void OnThemeChanged(AvaloniaPropertyChangedEventArgs<ThemeVariantEnum> eventArgs)
         {
-            StyledElement sender = (StyledElement)eventArgs.Sender;
+            AvaloniaObject sender = eventArgs.Sender;
+
+            if (sender is not IThemeVariantHost)
+            {
+                throw new Exception("Coding ERROR: ThemeVariantBehavior.TheThemeVariant attached property can only be set on an IThemeVariantHost");
+            }
 
             sender.SetCurrentValue(ThemeVariant.RequestedThemeVariantProperty, GetTheThemeVariant(sender).ToTheme());
         }
