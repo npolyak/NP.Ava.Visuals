39ddcb6ba251a818ae72fe5e012c6497f69862d2
npolyak <nick.polyak@verizon.net>
2024-06-19 14:51:26 -0400

    added most of the lines

---
+++ src/Controls/VisualsRepeater.cs
@@ -0,0 +1,37 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Styling;
+using System;
+
+namespace NP.Ava.Visuals.Controls
+{
+    public class VisualsRepeater : ItemsControl
+    {
+        protected override Type StyleKeyOverride => typeof(ItemsControl);
+
+        #region NumberOfTimesToRepeat Styled Avalonia Property
+        public int NumberOfTimesToRepeat
+        {
+            get { return GetValue(NumberOfTimesToRepeatProperty); }
+            set { SetValue(NumberOfTimesToRepeatProperty, value); }
+        }
+
+        public static readonly StyledProperty<int> NumberOfTimesToRepeatProperty =
+            AvaloniaProperty.Register<VisualsRepeater, int>
+            (
+                nameof(NumberOfTimesToRepeat)
+            );
+        #endregion NumberOfTimesToRepeat Styled Avalonia Property
+
+        IDisposable _subscription;
+        public VisualsRepeater()
+        {
+            _subscription = this.GetObservable<int>(NumberOfTimesToRepeatProperty).Subscribe(OnNumberOfTimesToRepeatPropertyChanged);
+        }
+
+        private void OnNumberOfTimesToRepeatPropertyChanged(int numberOfTimesToRepeat)
+        {
+            this.ItemsSource = new bool[numberOfTimesToRepeat];
+        }
+    }
+}
