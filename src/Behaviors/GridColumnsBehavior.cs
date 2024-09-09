6d510bab5252dd883a6fcb9da13ac0fe7e3ce726
npolyak <nick.polyak@verizon.net>
2024-09-09 09:19:29 +0200

    committing current state

---
+++ src/Behaviors/GridColumnsBehavior.cs
@@ -0,0 +1,44 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using System;
+
+namespace NP.Ava.Visuals.Behaviors
+{
+    public static class GridColumnsBehavior
+    {
+
+        #region ColDefsToSet Attached Avalonia Property
+        public static ColumnDefinitions GetColDefsToSet(Grid obj)
+        {
+            return obj.GetValue(ColDefsToSetProperty);
+        }
+
+        public static void SetColDefsToSet(Grid obj, ColumnDefinitions value)
+        {
+            obj.SetValue(ColDefsToSetProperty, value);
+        }
+
+        public static readonly AttachedProperty<ColumnDefinitions> ColDefsToSetProperty =
+            AvaloniaProperty.RegisterAttached<Grid, Grid, ColumnDefinitions>
+            (
+                "ColDefsToSet"
+            );
+
+        static GridColumnsBehavior()
+        {
+            ColDefsToSetProperty.Changed.Subscribe(OnColDefsChanged);
+
+
+        }
+
+        private static void OnColDefsChanged(AvaloniaPropertyChangedEventArgs<ColumnDefinitions> args)
+        {
+            Grid g = (Grid)args.Sender;
+
+            g.ColumnDefinitions.Clear();
+
+            g.ColumnDefinitions.AddRange(GetColDefsToSet(g));
+        }
+        #endregion ColDefsToSet Attached Avalonia Property
+    }
+}
