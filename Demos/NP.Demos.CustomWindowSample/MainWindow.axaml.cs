c3674a08357afe7ef0144d5e405c1e6419db7087
npolyak <nick.polyak@verizon.net>
2024-09-17 12:13:36 +0200

    created functionality to test moving captured pointer on mac

--- Demos/NP.Demos.CustomWindowSample/MainWindow.axaml.cs
+++ Demos/NP.Demos.CustomWindowSample/MainWindow.axaml.cs
@@ -1,12 +1,32 @@
+global using Point2D = NP.Utilities.Point2D<double>;
+
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
 using Avalonia.Markup.Xaml;
+using NP.Ava.Visuals.Behaviors;
 using NP.Ava.Visuals.Controls;
+using NP.Utilities;
+using System;
 
 namespace NP.Demos.CustomWindowSample
 {
     public partial class MainWindow : CustomWindow
     {
+        #region CurrentCapturedPointerPositionInScreen Styled Avalonia Property
+        public Point2D CurrentCapturedPointerPositionInScreen
+        {
+            get { return GetValue(CurrentCapturedPointerPositionInScreenProperty); }
+            set { SetValue(CurrentCapturedPointerPositionInScreenProperty, value); }
+        }
+
+        public static readonly StyledProperty<Point2D> CurrentCapturedPointerPositionInScreenProperty =
+            AvaloniaProperty.Register<MainWindow, Point2D>
+            (
+                nameof(CurrentCapturedPointerPositionInScreen)
+            );
+        #endregion CurrentCapturedPointerPositionInScreen Styled Avalonia Property
+
 
         #region WindowPosition Styled Avalonia Property
         public PixelPoint WindowPosition
@@ -29,13 +49,30 @@ namespace NP.Demos.CustomWindowSample
 #if DEBUG
             this.AttachDevTools();
 #endif
+        }
 
-            this.PositionChanged += MainWindow_PositionChanged;
+        protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
+        {
+            base.OnApplyTemplate(e);
+
+            this.PointerPressed += MainWindow_PointerPressed;
+        }
+
+        private void MainWindow_PointerPressed(object? sender, Avalonia.Input.PointerPressedEventArgs e)
+        {
+            CurrentScreenPointBehavior.Capture(HeaderControl, true, e);
+
+            CurrentScreenPointBehavior.CurrentScreenPoint.Subscribe(OnScreenPositionChanged);
+        }
+
+        private void OnScreenPositionChanged(Point2D<double> d)
+        {
+            CurrentCapturedPointerPositionInScreen = d;
         }
 
         private void MainWindow_PositionChanged(object? sender, PixelPointEventArgs e)
         {
-            WindowPosition = Position;
+            //WindowPosition = Position;
         }
 
         private void InitializeComponent()
