243486341d87fc7530dba381070a59b9174f2c44
npolyak <nick.polyak@verizon.net>
2024-09-26 16:00:01 +0200

    made the pin panel resizable.

---
+++ src/Converters/FirstOrSecondValueConverter.cs
@@ -0,0 +1,31 @@
+﻿using Avalonia.Controls;
+using Avalonia.Data.Converters;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace NP.Ava.Visuals.Converters
+{
+    public class FirstOrSecondValueConverter : IMultiValueConverter
+    {
+        public static FirstOrSecondValueConverter Instance { get; } = new FirstOrSecondValueConverter();
+
+        public object? Convert(IList<object?> values, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (values == null || values.Count < 2) 
+                return null;
+
+            if (values[0] is bool b)
+            {
+                if (b)
+                    return values[1];
+                else
+                {
+                    return (values.Count > 2) ? values[2] : null;
+                }
+            }
+
+            return null;
+        }
+    }
+}
